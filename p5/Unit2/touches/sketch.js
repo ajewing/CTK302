f3399eba2aae2e203b57e1463279110ee9e814d2
ajewing <59931537+ajewing@users.noreply.github.com>
2020-10-06 00:35:21 -0500

    go

--- p5/Unit2/touches/sketch.js
+++ p5/Unit2/touches/sketch.js
@@ -1,7 +1,39 @@
+let numberOfTouches ;
+
 function setup() {
-createCanvas(500, 500);
+  createCanvas(400, 400);
+  imageMode(CENTER);
 }
 
 function draw() {
+  clear();
+  numberOfTouches = touches.length;
+  text(numberOfTouches + ' touches', 5, 10);
+
+  switch(numberOfTouches) {
+    case 0:
+      text("no one is touching the screen", 5, 22) ;
+      break ;
+
+      case 1:
+       text("it's kind of lonely here", 5, 22) ;
+      // put a picture here
+      img1 = loadImage("assets/1.jpg");
+      break ;
+
+      case 2:
+      text("two fingers are touching the screen", 5, 22) ;
+            // put a picture here
+      img1 = loadImage("assets/2.jpg");
+      break ;
+
+      case 3:
+     text("three are touching the screen", 5, 22) ;
+            // put a picture here
+      img1 = loadImage("assets/3.jpg");
+      break ;
+
+
+  }
 
 }
